285a7c0368fd8fda78976dbfb5e139b227ed5d8a
Pablo Trinidad <pabloatp0204@gmail.com>
2017-03-11 21:05:20 -0700

    Ansible type fixed and manage.py file added.

---
+++ {{cookiecutter.project_slug}}/manage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Django entry point."""
+
+import os
+import sys
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        try:
+            import django  # NOQA
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
+    execute_from_command_line(sys.argv)
